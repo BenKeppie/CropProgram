95863336bf3da731a9d6f0b72413bc576b11bea0
BenKeppie <clockworkskateteam@gmail.com>
2014-06-14 13:41:59 +0100

    Auto grow function finished.

--- CropClass.py
+++ CropClass.py
@@ -1,3 +1,5 @@
+import random
+
 class Crop:
     """A class to represent a generic food crop"""
     #Constructor (Run automatically)
@@ -13,13 +15,45 @@ class Crop:
         self._status="Seed"
         self._type="Generic"
 
+    #Methods: functions inside class.
+
+    def needs(self):
+        #returns a dictionary containing light needs and and water needs.
+        return {"light need":self._light_need, "water need":self._water_need}
+    def report(self):
+        return {"type":self._type,"status":self._status,"growth":self._growth,"days growing":self._days_growing}
+
+    def _update_status(self):
+        if self._growth>15:
+            self._status="Old"
+        elif self._growth>10:
+            self._status="Mature"
+        elif self._growth>5:
+            self._status="Young"
+        elif self._growth>0:
+            self._status="Seedling"
+        elif self._growth==0:
+            self._status="Seed"
+        
+
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth+=self._growth_rate
+        self._days_growing += 1
+        self._update_status()
+
+
+def auto_grow(crop, days):
+    for day in range(days):
+        light=random.randint(1,10)
+        water=random.randint(1,10)
+        crop.grow(light,water)
 
 if __name__== "__main__":
     new_crop=Crop(1,4,3)
-    print(new_crop._status)
-    print(new_crop._light_need)
-    print(new_crop._water_need)
-    new_crop2=Crop(2,5,7)
-    print(new_crop2._status)
-    print(new_crop2._light_need)
-    print(new_crop2._water_need)
+    print(new_crop.needs())
+    print(new_crop.report())
+    auto_grow(new_crop,30)
+    print(new_crop.report())
+    
+    
