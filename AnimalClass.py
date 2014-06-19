f2ae0008d09347e2e2cbaaa52ad7ae21b59be359
BenKeppie <clockworkskateteam@gmail.com>
2014-06-19 17:55:15 +0100

    Attempt at animals
    All incorrect

---
+++ AnimalClass.py
@@ -0,0 +1,9 @@
+import random
+from CropClass import *
+
+class Animal:
+    """A class to represent animals"""
+    def __init__(self):
+        self._weight=self._growth
+        self._food_need=self._water_need
+        self._name=""
